b5e0722ac233e2d10a9fb0b32f123d0cc2c31cc9
Kaleeswaran V <vkalees64@gmail.com>
2025-01-04 04:52:58 +0530

    feat: sweetalert2 update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'vite-angular-tuto';
+
+  openAlert() {
+    Swal.fire(
+      'I am SweetAlert',
+      'Here i am using sweetalert for popup',
+      'success'
+    );
+  }
 }
