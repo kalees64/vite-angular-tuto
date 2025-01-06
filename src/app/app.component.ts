cbfe09baf89309e3095466ce8d4ff072e0b6ec8c
Kaleeswaran V <vkalees64@gmail.com>
2025-01-07 05:00:16 +0530

    feat: toatr and sonner update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import Swal from 'sweetalert2';
+import { toast, NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NgxSonnerToaster],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'vite-angular-tuto';
+
+  constructor(private toastr: ToastrService) {}
 
   openAlert() {
     Swal.fire(
@@ -17,5 +21,13 @@ export class AppComponent {
       'Here i am using sweetalert for popup',
       'success'
     );
+  }
+
+  openToastr() {
+    this.toastr.success('We got it!');
+  }
+
+  openSonner() {
+    toast.success('We got it Again!');
   }
 }
